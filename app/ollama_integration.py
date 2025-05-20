0f0c23b5fabf8057262bec16ed663eabda4ff542
ebythomas23 <169046811+ebythomas23@users.noreply.github.com>
2025-05-20 22:22:22 +1000

    Add ollama_integration.py for querying LLM with user context and query

---
+++ app/ollama_integration.py
@@ -0,0 +1,25 @@
+import ollama
+
+#combines user query and context to create a prompt ans send it to the llm , and returns the response
+def query_ollama(query,context, model_name="llama2"):
+  prompt=f"""
+    Use the following contect to answer the question
+
+    Context: {context}
+    Question: {query}
+    Answer:
+  """
+  #send the prompt to the LLM and get the response
+  try:
+    response= ollama.chat(model=model_name,
+                          message=[{"role":"user","content":prompt}])
+    return response['message']['content']
+  except Exception as e:
+    return f"Error:{str(e)}"
+  
+
+
+  #this is the Generation part of RAG.
+  #It takes the user query and the context (retrieved from the vector store) and sends it to the LLM (Ollama) to generate a response.
+ #The function constructs a prompt by combining the context and the query, and then sends it to the LLM using the ollama.chat method.    
+ #The response from the LLM is returned as the final answer to the user.
